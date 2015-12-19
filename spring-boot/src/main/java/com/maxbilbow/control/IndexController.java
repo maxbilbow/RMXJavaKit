04a4d7667933e78a81fedfa66d02c926c2872bb2
maxbilbow <max@rattlemedia.co.uk>
2015-12-19 17:54:05 +0000

    Springboot tag no longer crashes non-spring launch

--- spring-boot/src/main/java/com/maxbilbow/control/IndexController.java
+++ spring-boot/src/main/java/com/maxbilbow/control/IndexController.java
@@ -3,6 +3,7 @@ package com.maxbilbow.control;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Max on 19/12/2015.
@@ -16,4 +17,17 @@ public class IndexController {
     {
         return "Hello World";
     }
+
+    @RequestMapping("/string")
+    public String getString()
+    {
+        return "Hello, World!";
+    }
+
+    @RequestMapping("/ftl")
+    public ModelAndView getFtl()
+    {
+        return new ModelAndView("index");
+    }
+
 }
