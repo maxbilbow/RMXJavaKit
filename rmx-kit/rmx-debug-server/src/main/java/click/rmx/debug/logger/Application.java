a9ad32c0fa9573acbbaff3d528a077b17f4d84d2
maxbilbow <max@rattlemedia.co.uk>
2015-12-19 15:53:01 +0000

    Springboot tag no longer crashes non-spring launch

--- rmx-kit/rmx-debug-server/src/main/java/click/rmx/debug/logger/Application.java
+++ rmx-kit/rmx-debug-server/src/main/java/click/rmx/debug/logger/Application.java
@@ -1,44 +1,32 @@
 package click.rmx.debug.logger;
 
-import click.rmx.debug.logger.config.DBConfig;
-//import click.rmx.debug.logger.config.WebConfig;
+import click.rmx.debug.logger.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import javax.servlet.ServletRegistration;
 import java.nio.charset.StandardCharsets;
-import java.util.Locale;
+
+//import click.rmx.debug.logger.config.WebConfig;
 
 /**
  * Created by bilbowm on 23/10/2015.
  */
 @SpringBootApplication
-//@EnableAutoConfiguration
-//    @Configuration
 @EnableWebMvc
-@Import({
-        DBConfig.class//, EndpointConfig.class
-})
-@ComponentScan(basePackages = "click.rmx.debug.logger")
 public class Application
-        extends WebMvcConfigurerAdapter
-        implements WebApplicationInitializer,
-        WebMvcConfigurer
+//        extends WebMvcConfigurerAdapter
+//        implements WebApplicationInitializer,
+//        WebMvcConfigurer,
+implements ServletContextListener
 {
 
 
@@ -46,36 +34,34 @@ public class Application
     {
 //        onStartup();
     }
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver resolver = new SessionLocaleResolver();
-        resolver.setDefaultLocale(Locale.ENGLISH);
-        return resolver;
+
+
+
+    private CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding(StandardCharsets.UTF_8.name());
+        return characterEncodingFilter;
     }
 
 
-    @Bean
-    public InternalResourceViewResolver getInternalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/pages/");
-        resolver.setSuffix(".jsp");
-
-
-        return resolver;
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class,args);
     }
 
     @Override
-//    @Autowired
-    public void onStartup(ServletContext servletContext) throws ServletException
+    public void contextInitialized(ServletContextEvent servletContextEvent)
     {
         final AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();//getContext();
 
-        context.register(Application.class);
+        context.register(WebConfig.class);
 
-        servletContext.addListener(new ContextLoaderListener(context)); //ContextLoadListener is here
+        final ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
-
 
         //Add Mappings
         dispatcher.addMapping("*.html");
@@ -84,41 +70,11 @@ public class Application
         dispatcher.addMapping("*.js");
         dispatcher.addMapping("*.css");
 
-        //Filter mappings (is this needed?)
-        servletContext.addFilter("characterEncodingFilter", characterEncodingFilter());
-
-        //for webSockets
-        dispatcher.setInitParameter("dispatchOptionsRequest", "true");
-        dispatcher.setAsyncSupported(true);
-
-    }
-
-    private CharacterEncodingFilter characterEncodingFilter() {
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding(StandardCharsets.UTF_8.name());
-        return characterEncodingFilter;
-    }
-
-    /**
-     * Setup a simple strategy: use all the defaults and return XML by default when not sure.
-     */
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        super.configureContentNegotiation(configurer);
-        configurer.mediaType("json", MediaType.APPLICATION_JSON);
-//        configurer.mediaType("xml", MediaType.APPLICATION_XML);
-//        configurer.mediaType("txt", MediaType.APPLICATION_OCTET_STREAM);
     }
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
-        registry.addResourceHandler("/css/**").addResourceLocations("/assets/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/assets/js/");
-    }
-
-    public static void main(String[] args)
+    public void contextDestroyed(ServletContextEvent servletContextEvent)
     {
-        SpringApplication.run(Application.class,args);
+
     }
 }
--- rmx-kit/rmx-debug-server/src/main/java/click/rmx/debug/logger/config/WebConfig.java
+++ rmx-kit/rmx-debug-server/src/main/java/click/rmx/debug/logger/config/WebConfig.java
@@ -1,15 +1,107 @@
 package click.rmx.debug.logger.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.annotation.WebListener;
+import java.util.Locale;
+
 /**
  * Created by bilbowm on 23/10/2015.
  */
-//@Configuration
-////@EnableWebMvc
-//@Import({
-//        DBConfig.class//, EndpointConfig.class
-//})
-//@ComponentScan(basePackages = "click.rmx.debug.logger")
-//public class WebConfig  {
+@Configuration
+//@EnableWebMvc
+@Import({
+        DBConfig.class//, EndpointConfig.class
+})
+@WebListener
+@ComponentScan(basePackages = "click.rmx.debug.logger")
+public class WebConfig
+        extends WebMvcConfigurerAdapter
+        implements WebApplicationInitializer
+{
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
+    }
+
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+
+
+
+        return resolver;
+    }
+
+    /**
+     * Setup a simple strategy: use all the defaults and return XML by default when not sure.
+     */
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        super.configureContentNegotiation(configurer);
+        configurer.mediaType("json", MediaType.APPLICATION_JSON);
+//        configurer.mediaType("xml", MediaType.APPLICATION_XML);
+//        configurer.mediaType("txt", MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/assets/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/assets/js/");
+    }
+
+    @Override
+//    @Autowired
+    public void onStartup(ServletContext servletContext) throws ServletException
+    {
+        final AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();//getContext();
+
+        context.register(WebConfig.class);
+
+        servletContext.addListener(new ContextLoaderListener(context)); //ContextLoadListener is here
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
 //
-//
-//}
+
+//        ServletRegistration dispatcher = servletContext.getServletRegistration("dispatcherServlet");
+        //Add Mappings
+        dispatcher.addMapping("*.html");
+        dispatcher.addMapping("*.json");
+        dispatcher.addMapping("*.xml");
+        dispatcher.addMapping("*.js");
+        dispatcher.addMapping("*.css");
+
+
+        //Filter mappings (is this needed?)
+//        servletContext.addFilter("characterEncodingFilter", characterEncodingFilter());
+
+        //for webSockets
+        dispatcher.setInitParameter("dispatchOptionsRequest", "true");
+        dispatcher.setAsyncSupported(true);
+
+    }
+}
